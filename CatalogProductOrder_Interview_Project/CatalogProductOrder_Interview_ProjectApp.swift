78699ed33026456bc26816ce500e46be0d684750
Nate Mann <nathan.mann@me.com>
2024-08-09 13:43:51 -0400

    Initial Commit

---
+++ CatalogProductOrder_Interview_Project/CatalogProductOrder_Interview_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogProductOrder_Interview_ProjectApp.swift
+//  CatalogProductOrder_Interview_Project
+//
+//  Created by Nate Mann on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CatalogProductOrder_Interview_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatalogProductOrder_Interview_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatalogProductOrder_Interview_Project
+//
+//  Created by Nate Mann on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
