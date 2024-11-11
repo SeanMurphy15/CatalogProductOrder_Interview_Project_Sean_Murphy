fe980ff761b2aa2d3589e5acef88e324700030cb
Sean Murphy <Sean.Murphy@polaris.com>
2024-11-10 19:55:38 -1000

    going with environment view model

---
+++ CatalogProductOrder_Interview_Project/CatalogProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogProductDetailView.swift
+//  CatalogProductOrder_Interview_Project
+//
+//  Created by Sean Murphy on 11/10/24.
+//
+
+import SwiftUI
+
+struct CatalogProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatalogProductDetailView()
+}
---
+++ CatalogProductOrder_Interview_Project/CatalogProductRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CatalogProductRowView.swift
+//  CatalogProductOrder_Interview_Project
+//
+//  Created by Sean Murphy on 11/10/24.
+//
+
+import SwiftUI
+
+struct CatalogProductRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatalogProductRowView()
+}
